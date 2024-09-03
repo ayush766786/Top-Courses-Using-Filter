9655b3f83d475fbac6e58e2e63f935007132fc6c
ayush766786 <jayushkumar211@gmail.com>
2024-09-03 08:38:09 +0530

    main file pushed

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import Navbar from './Components/Navbar'
+import Filter from './Components/Filter'
+import Cards from './Components/Cards'
+import { apiUrl, filterData } from './Data'
+import './App.css'
+import { useEffect, useState } from 'react'
+import { toast } from 'react-toastify'
+
+function App() {
+  const [courses, setCourses] = useState('')
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch(apiUrl);
+        const output = await res.json();
+
+        //save data into a variable
+        // console.log(data)
+        setCourses(output.data)
+      } catch (error) {
+        toast.error("Something went wrong")
+      }
+    }
+    fetchData();
+  }, [])
+  return (
+    <>
+      <div>
+        <Navbar></Navbar>
+        <Filter  filterData={filterData} />
+        <Cards courses={courses}/>
+      </div>
+
+    </>
+  )
+}
+
+export default App
