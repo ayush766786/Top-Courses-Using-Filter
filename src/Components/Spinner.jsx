50337208072f7700424501250170c4293d054f59
ayush766786 <jayushkumar211@gmail.com>
2024-09-03 17:09:27 +0530

    Spinner added

---
+++ src/Components/Spinner.jsx
@@ -0,0 +1,14 @@
+import "./Spinner.css"
+
+const Spinner = () => {
+  return <>
+    <div className="main-spinner">
+      <div className="spinner"></div>
+      <p>Loading...</p>
+    </div>
+
+
+
+  </>
+}
+export default Spinner;
